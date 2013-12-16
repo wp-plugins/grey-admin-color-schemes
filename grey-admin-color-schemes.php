b8c72f40a7557f4d43e68db8f679f47f9fd6af06
GregRoss <GregRoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-16 20:53:39 +0000

    - Initial upload to WordPress.org
    git-svn-id: https://plugins.svn.wordpress.org/grey-admin-color-schemes/trunk@823404 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ grey-admin-color-schemes.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Gray Admin Color Scheme
+Plugin URI: http://wordpress.org/plugins/grey-admin-color-schemes
+Description: Grey color schemes for WordPress 3.8's admin area.
+Version: 1.0
+Author: Greg Ross
+Author URI: http://ToolStack.com/
+
+Compatible with WordPress 3.8+.
+
+Read the accompanying readme.txt file for instructions and documentation.
+
+Copyright (c) 2013 by Greg Ross
+
+This software is released under the GPL v2.0, see license.txt for details
+*/
+
+/*
+	Register color schemes.
+*/
+function GACSAddColorSchemes() 
+	{
+	wp_admin_css_color(	'classicgrey', __('Classic Grey'), plugins_url("classic/colors.css", __FILE__ ), array( '#464646', '#6D6D6D', '#F1F1F1', '#21759B' ) );
+	wp_admin_css_color(	'moderngrey', __('Modern Grey'), plugins_url("modern/colors.css", __FILE__ ), array( '#464646', '#6D6D6D', '#D6D6D6', '#21759B' ) );
+	}
+
+add_action( 'admin_init' , 'GACSAddColorSchemes' );
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Grey Admin Color Schemes ===
+Contributors: GregRoss
+Plugin URI: http://toolstack.com/grey-admin-color-schemes
+Author URI: http://toolstack.com
+Tags: admin color scheme
+Requires at least: 3.8.0
+Tested up to: 3.8.0
+Stable tag: 1.0
+License: GPLv2
+
+Grey color schemes for WordPress 3.8's admin area.
+
+== Description ==
+
+Grey color schemes for the admin area of WordPress 3.8.
+
+== Installation ==
+
+1. Extract the archive file into your plugins directory in the grey-admin-color-schemes folder.
+2. Activate the plugin in the Plugin options.
+3. Login to WordPress and go to your profile page and choose a grey color scheme to use.
+
+== Frequently Asked Questions ==
+
+= None at this time =
+
+== Changelog ==
+= 1.0 =
+* Initial release.
+
+== Upgrade Notice == 
+= 1.0 =
+* None.
+
+== Screenshots ==
+
+1. Classic Grey
+2. Modern Grey
