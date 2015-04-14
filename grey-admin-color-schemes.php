a7e5aeb442ec4ce20b9bb90d924d4e4210af025e
GregRoss <GregRoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 12:32:37 +0000

    - Added text domain for gettext calls.
    git-svn-id: https://plugins.svn.wordpress.org/grey-admin-color-schemes/trunk@1134477 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- grey-admin-color-schemes.php
+++ grey-admin-color-schemes.php
@@ -6,6 +6,8 @@ Description: Grey color schemes for WordPress 3.8's admin area.
 Version: 1.6
 Author: Greg Ross
 Author URI: http://toolstack.com/
+Text Domain: grey-admin-color-schemes
+Domain Path: /languages/
 
 Compatible with WordPress 3.8+.
 
@@ -23,10 +25,20 @@ function GACSAddColorSchemes()
 	{
 	$rtl = is_rtl() ? '-rtl' : '';
 
-	wp_admin_css_color(	'classicgrey', __('Classic Grey'), plugins_url("classic/colors$rtl.css", __FILE__ ), array( '#464646', '#6D6D6D', '#F1F1F1', '#21759B' ) );
-	wp_admin_css_color(	'moderngrey', __('Modern Grey'), plugins_url("modern/colors$rtl.css", __FILE__ ), array( '#464646', '#6D6D6D', '#D6D6D6', '#21759B' ) );
-	wp_admin_css_color( 'atouchofblue', __('A Touch of Blue'), plugins_url("atouchofblue/colors$rtl.css", __FILE__ ), array( '#464646', '#6D6D6D', '#D6D6D6', '#0074A2' ) );
-	wp_admin_css_color( 'postmodern', __('Post Modern Grey'), plugins_url("postmodern/colors$rtl.css", __FILE__ ), array( '#464646', '#6D6D6D', '#D6D6D6', '#F1F1F1' ) );
+	wp_admin_css_color(	'classicgrey', __('Classic Grey', 'grey-admin-color-schemes'), plugins_url("classic/colors$rtl.css", __FILE__ ), array( '#464646', '#6D6D6D', '#F1F1F1', '#21759B' ) );
+	wp_admin_css_color(	'moderngrey', __('Modern Grey', 'grey-admin-color-schemes'), plugins_url("modern/colors$rtl.css", __FILE__ ), array( '#464646', '#6D6D6D', '#D6D6D6', '#21759B' ) );
+	wp_admin_css_color( 'atouchofblue', __('A Touch of Blue', 'grey-admin-color-schemes'), plugins_url("atouchofblue/colors$rtl.css", __FILE__ ), array( '#464646', '#6D6D6D', '#D6D6D6', '#0074A2' ) );
+	wp_admin_css_color( 'postmodern', __('Post Modern Grey', 'grey-admin-color-schemes'), plugins_url("postmodern/colors$rtl.css", __FILE__ ), array( '#464646', '#6D6D6D', '#D6D6D6', '#F1F1F1' ) );
 	}
 
 add_action( 'admin_init' , 'GACSAddColorSchemes' );
+
+// Load the internationalization code.
+function GACSInit() {
+	load_plugin_textdomain('grey-admin-color-schemes', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/');
+	__('Grey Admin Color Schemes', 'grey-admin-color-schemes');
+	__('Grey color schemes for WordPress 3.8\'s admin area.', 'grey-admin-color-schemes');
+}
+
+// Add actions
+add_action('init', 'GACSInit');
